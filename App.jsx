29ee09674b2e8fcfdd89f01593efbbba5d283358
epione88 <92001523+epione88@users.noreply.github.com>
2026-06-08 20:38:52 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/*.png'],
+      manifest: {
+        name: 'FitLog',
+        short_name: 'FitLog',
+        description: 'Tabata workout tracker with progressive overload',
+        theme_color: '#0f0f14',
+        background_color: '#0f0f14',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/world\.openfoodfacts\.org\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'food-api-cache',
+              expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 }
+            }
+          }
+        ]
+      }
+    })
+  ]
+})
